1c2097b31ed9b7dda0e7b1b55d5b12da09789258
zloy-youPich <zlyoupich@gmail.com>
2023-12-12 22:08:32 +0200

    vahui3

--- main/urls.py
+++ main/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('goods', show_goods, name='goods'),
-    path('', default_page, name='default_page')
+    path('', default_page, name='default_page'),
+    path('cart/', cart, name='cart')
 ]
